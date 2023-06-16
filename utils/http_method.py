a5ef38a151eaae2d960ca1ba349acaf8aa0343ff
Feklan <105582792+Feklan@users.noreply.github.com>
2023-06-17 00:04:49 +0300

    logger

--- utils/http_method.py
+++ utils/http_method.py
@@ -1,4 +1,7 @@
 import requests
+
+from utils.logger import Logger
+
 """Список hhtp методов"""
 class Http_methods:
     headers = {'Content-Type' : 'application/json'}
@@ -6,20 +9,28 @@ class Http_methods:
 
     @staticmethod
     def get(url):
+        Logger.add_request(url, method="GET")
         result = requests.get(url, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def post(url, body):
+        Logger.add_request(url, method="POST")
         result = requests.post(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def put(url, body):
+        Logger.add_request(url, method="PUT")
         result = requests.put(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, method="DELETE")
         result = requests.put(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
